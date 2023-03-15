6ffb6b2fb2854a9bceef543783f01041d37c02fd
giuliabertoldii <127933600+giuliabertoldii@users.noreply.github.com>
2023-03-15 09:18:18 +0100

    Add files via upload

---
+++ query_db_spesa.sql
@@ -0,0 +1,43 @@
+DROP DATABASE if EXISTS db_spesa;
+CREATE DATABASE if NOT EXISTS db_spesa;
+USE db_spesa;
+
+CREATE TABLE utente(
+id_utente INT AUTO_INCREMENT PRIMARY KEY,
+nome VARCHAR(30) NOT NULL,
+cognome VARCHAR(30) NOT NULL,
+email VARCHAR(50) NOT NULL,
+pass VARCHAR(250) NOT NULL);
+
+CREATE TABLE gruppo(
+id_gruppo INT AUTO_INCREMENT PRIMARY KEY,
+nome VARCHAR(30) NOT NULL,
+descrizione VARCHAR(200));
+
+CREATE TABLE famiglia(
+id_gruppo INT,
+id_utente INT,
+FOREIGN KEY(id_gruppo) REFERENCES gruppo(id_gruppo),
+FOREIGN KEY(id_utente) REFERENCES utente(id_utente));
+
+CREATE TABLE elenco(
+id_elenco INT AUTO_INCREMENT PRIMARY KEY,
+nome_elenco VARCHAR(30) NOT NULL,
+id_gruppo INT,
+data_elenco DATE,
+FOREIGN KEY(id_gruppo) REFERENCES gruppo(id_gruppo));
+
+CREATE TABLE lista(
+id_lista INT AUTO_INCREMENT PRIMARY KEY,
+nome_lista VARCHAR(30) NOT NULL,
+data_lista DATE,
+id_gruppo INT,
+FOREIGN KEY(id_gruppo) REFERENCES gruppo(id_gruppo));
+
+CREATE TABLE oggetto(
+id_oggetto INT AUTO_INCREMENT PRIMARY KEY,
+nome_oggetto VARCHAR(30) NOT NULL,
+note VARCHAR(200),
+controllo TINYINT,
+id_lista INT,
+FOREIGN KEY(id_lista) REFERENCES lista(id_lista));
