aee513237c1ab07df404d11f19912973ceaaf5db
pippo-san <ingev.stefano@gmail.com>
2023-04-28 09:30:49 +0200

    refactoring home

--- app/src/main/java/com/main/listify/Home.java
+++ app/src/main/java/com/main/listify/Home.java
@@ -1,16 +1,9 @@
 package com.main.listify;
 
-import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.TextView;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
@@ -23,41 +16,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.main.listify.databinding.ActivityHomeBinding;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 public class Home extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityHomeBinding binding;
 
-    private TextView txt_username;
-    private String username;
-    private Bundle passaggioDati;
-    private ListView listView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_home);
 
         binding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        setSupportActionBar(binding.appBarHome.toolbar);
-        FloatingActionButton fab=findViewById(R.id.fab);
 
-        fab.setOnClickListener(new View.OnClickListener() {
+        setSupportActionBar(binding.appBarHome.toolbar);
+        binding.appBarHome.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent apri_crea = new Intent(Home.this,MainActivity.class);
-                startActivity(apri_crea);
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
         DrawerLayout drawer = binding.drawerLayout;
@@ -71,32 +47,12 @@ public class Home extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_home);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
-
-        // Scritto
-        passaggioDati = getIntent().getExtras();
-
-        txt_username = ((TextView) findViewById(R.id.txt_nome_utente));
-        username = (String) passaggioDati.get("username");
-
-        // try {
-        //     Connection_helper.schermataHome(username.toString());
-        // } catch (IOException e) {
-        //     throw new RuntimeException(e);
-        // }
-
-        txt_username.setText(username);
-
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.home, menu);
-
-        txt_username = ((TextView) findViewById(R.id.txt_nome_utente));
-        username = (String) passaggioDati.get("username");
-
-        txt_username.setText(username);
         return true;
     }
 
@@ -105,11 +61,5 @@ public class Home extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_home);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
-    }
-
-    public void crea_lista_elenco(View view) {
-        Intent activity = new Intent(this, MainActivity.class);
-        activity.putExtra("username", username);
-        startActivity(activity);
     }
 }
