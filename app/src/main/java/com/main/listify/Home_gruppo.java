b20d56ce4c3968c81152f73c84c9c3a49ac1e8be
pippo-san <ingev.stefano@gmail.com>
2023-05-24 08:59:43 +0200

    aggiunta pagina home per il gruppo e gestione degli errori in essa

--- app/src/main/java/com/main/listify/Home_gruppo.java
+++ app/src/main/java/com/main/listify/Home_gruppo.java
@@ -2,47 +2,42 @@ package com.main.listify;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
+import android.view.Menu;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.drawerlayout.widget.DrawerLayout;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.navigation.NavigationView;
-import com.main.listify.databinding.ActivityHomeBinding;
-
-import java.io.IOException;
+import com.main.listify.databinding.ActivityHomeGruppoBinding;
 
 public class Home_gruppo extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
-    private ActivityHomeBinding binding;
-
-    private TextView txt_username;
-    private TextView txt_nome_gruppo;
+    private ActivityHomeGruppoBinding binding;
 
     private String username;
+    private String idGruppo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        binding = ActivityHomeGruppoBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.appBarHome.toolbar);
-        binding.appBarHome.fab.setOnClickListener(new View.OnClickListener() {
+        setSupportActionBar(binding.appBarHomeGruppo.toolbar);
+        binding.appBarHomeGruppo.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent activity = new Intent(Home_gruppo.this, CreaGruppo.class);
-                activity.putExtra("username", username);
-                startActivity(activity);
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
         DrawerLayout drawer = binding.drawerLayout;
@@ -53,66 +48,44 @@ public class Home_gruppo extends AppCompatActivity {
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
                 .setOpenableLayout(drawer)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_home);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_home_gruppo);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
 
         // Scritto
+        Bundle passaggioDati = getIntent().getExtras();
         try {
-            Bundle passaggioDati = getIntent().getExtras();
             username = passaggioDati.get("username").toString();
+
+            // Se ho l'username, continuo
+            try {
+                idGruppo = passaggioDati.get("idGruppo").toString();
+            } catch (NullPointerException e) {
+                // Se per caso non ho l'idGruppo, rimando alla pagina Home
+                Intent activity = new Intent(this, Home.class);
+                activity.putExtra("username", username);
+                startActivity(activity);
+            }
         } catch (NullPointerException e) {
-            // Se per caso non ho l'username, rimando alla pagina di login
+            // Se per caso non ho l'username, rimando alla pagina di Login
             Intent activity = new Intent(this, Login.class);
             startActivity(activity);
         }
 
-        popolaTextView();
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home, menu);
-        TextView txt_username = (TextView)findViewById(R.id.txt_menu_username);
-        TextView txt_email = (TextView)findViewById(R.id.txt_menu_email);
-        txt_username.setText(username);
-        try {
-            txt_email.setText(Connection_helper.prendiRisultati("SELECT email FROM `utente` where username like \""+username+"\" "));
-        } catch (IOException e) {
-            txt_email.setText("Errore interno");
-        }
+        getMenuInflater().inflate(R.menu.home_gruppo, menu);
         return true;
     }
 
     @Override
     public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_home);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_home_gruppo);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
-    }
-
-    public void apriImpostazioni(MenuItem item) {
-        Intent activity = new Intent(this, SettingsActivity.class);
-        activity.putExtra("username", username);
-        startActivity(activity);
-    }
-
-    public void apriProfilo(MenuItem item) {
-        Intent activity = new Intent(this, pagina_profilo.class);
-        activity.putExtra("username", username);
-        startActivity(activity);
-        finish();
-    }
-
-
-    public void popolaTextView(){
-        txt_nome_gruppo = findViewById(R.id.query_nome_gruppo);
-
-        try {
-            txt_nome_gruppo.setText(Connection_helper.prendiRisultati("select nome from gruppo inner join famiglia on gruppo.id_gruppo=famiglia.id_gruppo where username like \""+username+"\" "));
-        } catch (Exception e) {
-            txt_nome_gruppo.setText("Errore interno");
-        }
     }
 }
